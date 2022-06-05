c93b70977d60fa8b490f1718ab2ff503a320dd10
Rcastro89 <r_castro_m@yahoo.es>
2022-06-05 13:35:18 -0500

    status js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,26 +1,42 @@
-$(document).ready(init);
-
-function init () {
-  const amenityObj = {};
-  $('.amenities .popover input').change(function () {
-    if ($(this).is(':checked')) {
-      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenityObj[$(this).attr('data-name')];
-    }
-    const names = Object.keys(amenityObj);
-    $('.amenities h4').text(names.sort().join(', '));
-  });
-
-  apiStatus();
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
+docReady(function() {
+  // DOM is loaded and ready for manipulation here
+  let objetos = {};
+  var checkbox = document.getElementsByClassName('pru');
+  for (let x = 0; x < checkbox.length; x++){
+    checkbox[x].addEventListener( 'change', function() {
+      if(checkbox[x].checked) {
+        objetos[checkbox[x].getAttribute('data-name')] = checkbox[x].getAttribute('data-id');
+      } else {
+        delete objetos[checkbox[x].getAttribute('data-name')]
+      }
+      let nombre = Object.keys(objetos);
+      document.getElementById('ame-select').innerHTML = nombre;
+    });
+  }
+
+  apiStatus();
+
+});
+
 function apiStatus () {
-  $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
-    if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://0.0.0.0:5001/api/v1/status/', false);
+  req.send(null);
+  if (req.status == 200){
+    let conectado = document.getElementById('api_status');
+    conectado.className = 'available';
+  } else {
+    let conectado = document.getElementById('api_status');
+    conectado.className = '';
+  }
 }
